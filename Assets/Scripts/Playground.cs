645cda5f1faa5cdbd44e0d3338a03317ab1cb3b5
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-30 23:02:13 +0300

    52. Bir script dosyasından başka bir script dosyasına ulaşmak

--- Assets/Scripts/Playground.cs
+++ Assets/Scripts/Playground.cs
@@ -4,13 +4,4 @@ using UnityEngine;
 
 public class Playground : MonoBehaviour
 {
-    public GameObject cube;
-    public GameObject target;
-
-    float speed = 10f;
-
-    private void Update()
-    {
-        cube.transform.position = Vector3.MoveTowards(cube.transform.position, target.transform.position, Time.deltaTime * speed);
-    }
 }
