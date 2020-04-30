709b01dc2917686ad2eb0e2979702797f4cc67d2
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-30 23:30:26 +0300

    56. Buton elemanını kullanmak

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text countText;
+    public int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countText.text = count.ToString();
+    }
+
+    public void Pressed()
+    {
+        count++;
+    }
+}
