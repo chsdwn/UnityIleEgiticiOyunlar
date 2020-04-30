29aef5313ffd984b21c46e670222759bf207e99b
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-30 22:05:35 +0300

    38 .Instantiate ve Invoke Fonksiyonları

---
+++ Assets/Scripts/Playground.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playground : MonoBehaviour
+{
+    public GameObject cubePrefab;
+
+    void Start()
+    {
+        // Runs CreateCube method after 2 seconds
+        Invoke("CreateCube", 2f);
+    }
+
+    private void CreateCube()
+    {
+        Instantiate(cubePrefab, transform.position, Quaternion.identity);
+    }
+}
