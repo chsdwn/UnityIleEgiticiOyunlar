3297e8bdb5513440eea7022bf309100ec89bf9f1
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-05-03 23:11:57 +0300

    59. Dotween ile UI elementlerine hareket verelim

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,26 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
-    public Text countText;
-    public int count;
+    public RectTransform panel1, panel2;
 
-    // Start is called before the first frame update
-    void Start()
+    public void Move()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        countText.text = count.ToString();
-    }
-
-    public void Pressed()
-    {
-        count++;
+        panel1.DOAnchorPos3D(new Vector3(480, 0, 0), 0.5f).SetEase(Ease.InBack);
+        panel2.DOAnchorPos3D(Vector3.zero, 0.5f).SetEase(Ease.OutBack).SetDelay(0.5f);
     }
 }
