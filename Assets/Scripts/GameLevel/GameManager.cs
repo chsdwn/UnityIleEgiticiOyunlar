2ff5b1e9ba7a25c38ddc7fa2bcbe56e7aec75a2b
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-05-04 00:13:50 +0300

    64. Game Level sahnesine kareleri dinamik olarak yerleştiriyoruz

---
+++ Assets/Scripts/GameLevel/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GameManager : MonoBehaviour
+{
+    private const int SIZE = 25;
+
+    [SerializeField]
+    private GameObject tilePrefab;
+    [SerializeField]
+    private Transform tiles;
+    private GameObject[] tilesArray = new GameObject[SIZE];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateTiles();
+    }
+
+    public void CreateTiles()
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            GameObject tile = Instantiate(tilePrefab, tiles);
+            tilesArray[i] = tile;
+        }
+
+        StartCoroutine(DoFadeCoroutine());
+    }
+
+    IEnumerator DoFadeCoroutine()
+    {
+        foreach (var tile in tilesArray)
+        {
+            tile.GetComponent<CanvasGroup>().DOFade(1, 0.07f);
+
+            yield return new WaitForSeconds(0.07f);
+        }
+    }
+}
