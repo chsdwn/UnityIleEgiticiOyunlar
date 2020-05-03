d6498e7a72474e2c9b4a6ce32c71d2651856865a
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-05-03 23:46:09 +0300

    62. Menü sahnesinin kodlamasını yapalım

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject startBtn, exitBtn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        FadeOut();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ExitGame()
     {
-        
+        Application.Quit();
+    }
+
+    public void StartGameLevel()
+    {
+        SceneManager.LoadScene("GameLevel");
+    }
+
+    private void FadeOut()
+    {
+        startBtn.GetComponent<CanvasGroup>().DOFade(1f, 0.8f);
+        exitBtn.GetComponent<CanvasGroup>().DOFade(1f, 0.8f).SetDelay(0.5f);
     }
 }
