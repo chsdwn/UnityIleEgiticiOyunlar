ddb843fd633dbe1ae113610c73f4a19c4597cc06
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-05-03 23:52:56 +0300

    63. Game Level sahnesini hazırlayalım

---
+++ Assets/Scripts/GameLevel/AlphaPanel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class AlphaPanel : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<CanvasGroup>().DOFade(0, 2f);
+    }
+}
