7ff646e56b6cd8c39e399a082bb011b429602813
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-05-10 01:42:56 +0300

    85.Game Level Sahnemizin Girişine Animasyon Ekleyelim

---
+++ Assets/Scripts/GameLevel/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject scoreTimePanel;
+    [SerializeField]
+    private GameObject catchPointsImage;
+    [SerializeField]
+    private GameObject topRect;
+    [SerializeField]
+    private GameObject bottomRect;
+
+
+    void Start()
+    {
+        UpdateScene();
+    }
+
+    void UpdateScene()
+    {
+        scoreTimePanel.GetComponent<CanvasGroup>().DOFade(1, 1f);
+        catchPointsImage.GetComponent<CanvasGroup>().DOFade(1, 1f);
+
+        topRect.GetComponent<RectTransform>().DOLocalMoveX(0, 0.7f).SetEase(Ease.OutBack);
+        bottomRect.GetComponent<RectTransform>().DOLocalMoveX(0, 0.7f).SetEase(Ease.OutBack);
+    }
+}
