e58e1c2b8ac289a44d5dbd04f54efc1ea83fc0e0
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-05-10 01:21:25 +0300

    82. Kafaya Ve Butona Hareket Verelim

---
+++ Assets/Scripts/MenuLevel/MenuManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform head;
+    [SerializeField]
+    private Transform startBtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        head.transform.GetComponent<RectTransform>().DOLocalMoveX(0f, 1f).SetEase(Ease.OutBack);
+        startBtn.transform.GetComponent<RectTransform>().DOLocalMoveY(-270f, 1f).SetEase(Ease.OutBack);
+    }
+}
