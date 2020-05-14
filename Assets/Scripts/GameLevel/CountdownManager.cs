cd61c4f57bb68fc8a516c45cd99ff8ad1a01ef1a
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-05-14 15:56:08 +0300

    86. Geri Sayım Fonksiyonunu Yazalım

---
+++ Assets/Scripts/GameLevel/CountdownManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class CountdownManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject countdownObj;
+    [SerializeField]
+    private Text countdownTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CountdownRoutine());
+    }
+
+    IEnumerator CountdownRoutine()
+    {
+        for (int i = 3; i > 0; i--)
+        {
+            countdownTxt.text = i.ToString();
+            yield return new WaitForSeconds(0.5f);
+
+            countdownObj.GetComponent<RectTransform>().DOScale(1, 0.5f).SetEase(Ease.OutBack);
+            yield return new WaitForSeconds(1f);
+
+            countdownObj.GetComponent<RectTransform>().DOScale(0, 0.5f).SetEase(Ease.InBack);
+            yield return new WaitForSeconds(0.6f);
+        }
+
+        StopAllCoroutines();
+    }
+}
