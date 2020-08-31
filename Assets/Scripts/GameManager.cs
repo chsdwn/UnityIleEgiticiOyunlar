d85c37c2e6c53ab85617f643d0e5aee5f699d324
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-08-31 14:03:41 +0300

    116. Soru Classını Yazalım

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Question[] questions { get; private set; }
+
+    void Start()
+    {
+        questions = new Question[] {
+            new Question { question = "Şarkılar her zaman beyitlerden oluşur.", isCorrect = false },
+            new Question { question = "Şarkı türünün en önemli ismi Nedim'dir.", isCorrect = true },
+            new Question { question = "Tuyuğ bir Türk nazım şeklidir.", isCorrect = true }
+        };
+    }
+}
